6b1851a7ce441bcfcd351d3a71ef3ccea1ca0848
Giselp <gisel.pediconi@hotmail.com>
2025-01-12 16:03:13 +0100

    Queries with prisma

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,84 @@
-const express = require('express');
+const express = require("express");
+const prisma = require("../prisma");
 const router = express.Router();
 
-router.get('/', (req, res) => {})
+router.get("/", async (req, res) => {
+  try {
+    const books = await prisma.Libro.findMany();
+    return res.json(books);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/with-sales", async (req, res) => {
+  try {
+    const books = await prisma.Libro.findMany({
+      select: {
+        ISBN: true,
+        Titulo: true,
+        Ventas: {
+          select: {
+            Cantidad: true,
+          },
+        },
+      },
+    });
+    const totalList = books
+      .map((book) => {
+        book.Ventas = book.Ventas.reduce((acc, curr) => acc + curr.Cantidad, 0);
+        return book;
+      })
+      .sort((x, y) => y.Ventas - x.Ventas);
+
+    return res.json(totalList[0]);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/:isbn", async (req, res) => {
+  try {
+    const isbn = req.params.isbn;
+    const libro = await prisma.Libro.findUnique({
+      where: {
+        ISBN: isbn,
+      },
+    });
+    return res.json(libro);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/author/:author", async (req, res) => {
+  try {
+    const author = req.params.author;
+    const libro = await prisma.Libro.findMany({
+      where: {
+        Autor: author,
+      },
+    });
+    return res.json(libro);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/price/:price", async (req, res) => {
+  try {
+    const price = Number(req.params.price);
+    const bookPrice = await prisma.Libro.findMany({
+      where: {
+        Precio: {
+          gt: price,
+        },
+      },
+    });
+    return res.json(bookPrice);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
--- routes/sales.js
+++ routes/sales.js
@@ -1,6 +1,74 @@
 const express = require("express");
+const prisma = require("../prisma");
 const router = express.Router();
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const sales = await prisma.Venta.findMany();
+    return res.json(sales);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const venta = await prisma.Venta.findUnique({
+      where: {
+        ID_Venta: id,
+      },
+    });
+    return res.json(venta);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/book/:isbn", async (req, res) => {
+  try {
+    const isbn = req.params.isbn;
+    const ventas = await prisma.Venta.findMany({
+      where: {
+        ISBN: isbn,
+      },
+    });
+    return res.json(ventas);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/date/:date", async (req, res) => {
+  try {
+    const date = req.params.date;
+    const inputDate = new Date(date);
+    const ventasPorDia = await prisma.Venta.findMany({
+      where: {
+        Fecha_Venta: inputDate,
+      },
+    });
+    return res.json(ventasPorDia);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//Endpoint `/sales/top`: Extrae el libro con el mayor ingreso total generado
+
+/* router.get("/top", async (req, res) => {
+  try{
+    const topBook = prisma.Venta.findMany({
+
+
+      include: {
+        Libro: true
+      }
+    
+    
+    })
+  }
+})
+ */
 
 module.exports = router;
